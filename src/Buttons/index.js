50f44322b5a6a439aa4066425217c112653b42d5
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-03-26 19:07:19 +0200

    length fix

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,20 +3,19 @@ import "./style.css";
 const Buttons = ({ tasks, hideDoneTasks, toggleHideDone, setAllDone }) => (
     <div className="buttons">
     {tasks.length > 0 && (
-    <>
-            <button onClick={toggleHideDone} className="buttons__button">
+        <>
+            <button className="buttons__button">
                 {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
-            <button 
-            onClick={setAllDone}
-            className="buttons__button"
-            disabled={tasks.every(({ done }) => done)}
+            <button
+                className="buttons__button"
+                disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
             </button>
         </>
     )}
-    </div>
+</div>
 );
 
 export default Buttons;
