539c819a288a4826f59e8f16e5e361ca16cf8fab
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-03-26 19:04:48 +0200

    misspeling

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks, toggleHideDone, setAllDone }) => (
     <div className="buttons">
-    {tasks.lenght > 0 && (
+    {tasks.length > 0 && (
     <>
             <button onClick={toggleHideDone} className="buttons__button">
                 {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
