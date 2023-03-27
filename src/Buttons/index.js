e03c7a62e7037450ce0212298b461893c4fc691e
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-03-27 20:25:33 +0200

    correcting the formatting in Buttons, Container $ Form

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,22 +2,17 @@ import "./style.css";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     <div className="buttons">
-    {tasks.length > 0 && (
-        <>
-            <button 
-            className="buttons__button"
-            >
-                {hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button
-                className="buttons__button"
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
-            </button>
-        </>
-    )}
-</div>
+        {tasks.length > 0 && (
+            <>
+                <button className="buttons__button">
+                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button className="buttons__button" disabled={tasks.every(({ done }) => done)}>
+                    Ukończ wszystkie
+                </button>
+            </>
+        )}
+    </div>
 );
 
 export default Buttons;
--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,7 @@
 import "./style.css";
 
 const Container = ({ children }) => (
-    <main 
-    className="container"
-    >
+    <main className="container">
         {children}
     </main>
 );
