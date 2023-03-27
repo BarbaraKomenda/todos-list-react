8191bbc2492578cc797bc77b25e86c694f1911ce
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-03-27 16:09:20 +0200

    replaced hideDoneTasks -> hideDone

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,13 +1,13 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDoneTasks, toggleHideDone, setAllDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     <div className="buttons">
     {tasks.length > 0 && (
         <>
             <button 
             className="buttons__button"
             >
-                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                {hideDone ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button
                 className="buttons__button"
