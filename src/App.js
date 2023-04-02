372889039340029a4d3d633601d3a843ed0bca6b
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-04-02 11:34:41 +0200

    added local storage

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
+const getInitialTasks = () => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+
+  return tasksFromLocalStorage
+    ? JSON.parse(tasksFromLocalStorage)
+    : [];
+};
 
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "odrobić pracę domową", done: false },
-    { id: 2, content: "przejść na Reacta", done: true },
-  ]);
+  const [tasks, setTasks] = useState(getInitialTasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
